d7a6a0f35c850810fd409d9e5e3b46386a0ebe7f
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-08-01 21:39:29 +0200

    feat: add simple hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,45 @@
+import axios from 'axios';
+import { useEffect } from 'react';
 import './App.css';
 import Editor from './Components/Editor';
+import samoraLang from "/samora-logo.png";
 
 const App = () => {
+  useEffect(() => {
+    const fetchData = () => {
+      axios.get(import.meta.env.VITE_TRIGGER_URL)
+        .then(response => {
+          console.log('Response:', response.data);
+        })
+        .catch(() => {
+          console.error('Error:');
+        });
+    };
+
+    fetchData();
+  });
+
   return (
     <>
+      <header id="header">
+        <div className="wrapper">
+          <div className="logo">
+            <img src={samoraLang} alt="" />
+          </div>
+        </div>
+      </header>
+      <main>
+        <section id="hero">
+          <div className="wrapper">
+            <div className="content">
+              <div className="text">
+                <span>Um novo paradigma de <span className="bold">Programação!</span></span>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+
       <div className="container">
         <h1>SamoraLang Playground</h1>
 
